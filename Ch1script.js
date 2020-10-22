e5da372a89ed4290fc5f0e7173db6482c3f5405d
Ivan <1ivankubacka@gmail.com>
2020-10-22 22:03:44 +0200

    uf

--- Ch1script.js
+++ Ch1script.js
@@ -6,6 +6,14 @@ $(function () {
         window.location.href = adresa;
 
     })
+    $('#findbut').click(function() {
+        var ix = $("#trans").prop("selectedIndex");
+        console.log(ix);
+        $('#DMTdiv').toggle(ix === 0);
+        $('#Ayawaskadiv').toggle(ix === 1);
 
+
+
+    });
 });
 
